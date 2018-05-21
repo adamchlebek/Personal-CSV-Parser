20fb784e547e23e85566372b9b6523e86dd01f90
Adam Chlebek <adamchlebek@live.com>
2018-05-21 11:15:01 -0400

    First Commit

---
+++ Personal CSV Parser/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Personal CSV Parser")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("greatexpressions.com")>
+<Assembly: AssemblyProduct("Personal CSV Parser")>
+<Assembly: AssemblyCopyright("Copyright © greatexpressions.com 2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("77e59498-a0d9-4319-8f80-a47397a146a0")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
